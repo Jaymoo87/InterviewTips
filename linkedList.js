35c03a7e52f9ac671f641acd5a6604cf69a4b2c5
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-12 11:23:00 -0500

    part 2 of linkedLists, destroy highlights some differences in array stucture vs. linkedlist structure

--- linkedList.js
+++ linkedList.js
@@ -8,6 +8,14 @@
  * insertHead(val)
  * inserAtIndex(val, index)
  * print()
+ *
+ *
+ * PART 2
+ * exists(val)
+ * destroyFirst()
+ * destroyLast(previousNode)
+ * destroyBetween(previousNode, node)
+ * remove(val)
  */
 
 class ListNode {
@@ -76,6 +84,58 @@ class LinkedList {
     }
     console.log(result);
   }
+
+  exists(val) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.val === val) {
+        console.log(val + " has been found");
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    console.log(val + " has not been found");
+
+    return false;
+  }
+
+  destroyFirst() {
+    let currentNode = this.head;
+    this.head = currentNode.next;
+  }
+
+  destroyLast(previousNode) {
+    previousNode.next = null;
+  }
+
+  destroyBetween(previousNode, node) {
+    previousNode.next = node.next;
+  }
+
+  remove(val) {
+    let currentNode = this.head;
+    let previousNode = this.head;
+
+    if (!this.isEmpty()) {
+      while (currentNode) {
+        if (currentNode.val === val) {
+          if (currentNode === this.head) {
+            this.destroyFirst();
+            return;
+          } else if (currentNode.next === null) {
+            this.destroyLast(previousNode);
+            return;
+          } else {
+            this.destroyBetween(previousNode, currentNode);
+            return;
+          }
+        }
+
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+    }
+  }
 }
 
 const list = new LinkedList();
@@ -85,4 +145,6 @@ list.insert("Grog");
 list.insert("Percy");
 list.insertHead("Pike");
 list.insertAtIndex("Vax", 3);
+list.print();
+list.remove("Vax");
 list.print();
